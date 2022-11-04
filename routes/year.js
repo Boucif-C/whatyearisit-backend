1caf78a4333154271b08b448af08521d9f11a119
Boucif <chabani.boucif@gmail.com>
2022-11-04 14:23:30 +0100

    Expected: 2020 to string

--- routes/year.js
+++ routes/year.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router= express.Router();
 
 router.get('/',(req,res)=>{
-    res.json({ year: new Date().getFullYear() })
+    console.log('Boucif')
+    res.json({ year: new Date().getFullYear().toString() })
 })
 
 module.exports=router;
